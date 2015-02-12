b7534e683d1c1ff1201bdfebb4eb232ad504b675
Adam Ginsburg <keflavich@yahoo.com>
2015-02-12 23:03:48 +0100

    fix up vmap_air and continued debugging of single_spectrum

--- muse/single_spectrum.py
+++ muse/single_spectrum.py
@@ -1,3 +1,4 @@
+import numpy as np
 from astropy.io import fits
 import spectral_cube
 
@@ -9,8 +10,89 @@ cube_125 = spectral_cube.SpectralCube.read('CUBEec_nall.fits', hdu=1)
 sp085 = cube_085[:,coordinate[1], coordinate[0]]
 sp125 = cube_125[:,coordinate[1], coordinate[0]]
 
-sp085.write("spectrum_{0}_{1}_085.fits".format(*coordinate))
-sp125.write("spectrum_{0}_{1}_125.fits".format(*coordinate))
+sp085.write("spectrum_{0}_{1}_085.fits".format(*coordinate), overwrite=True)
+sp125.write("spectrum_{0}_{1}_125.fits".format(*coordinate), overwrite=True)
 
 sp085.quicklook(None)
 sp125.quicklook(None)
+
+
+import pyspeckit
+import pylab as pl
+sp1 = pyspeckit.Spectrum("spectrum_{0}_{1}_085.fits".format(*coordinate))
+sp2 = pyspeckit.Spectrum("spectrum_{0}_{1}_125.fits".format(*coordinate))
+
+wavelengths = spectral_cube.spectral_axis.vac_to_air(np.array([6302.046,
+                                                               6313.8,
+                                                               6365.536,
+                                                               6549.85,
+                                                               6564.61,
+                                                               6585.28,
+                                                               6679.99556,
+                                                               6718.29,
+                                                               6732.67])*u.AA).value
+sp1.xarr.convert_to_unit('angstroms')
+sp2.xarr.convert_to_unit('angstroms')
+sp1.plotter()
+sp2.plotter(axis=sp1.plotter.axis, color='b', clear=False)
+sp1.plotter.line_ids([str(x) for x in range(len(wavelengths))], wavelengths, xval_units='angstroms')
+cont1 = sp1.slice(6380, 6500, units='angstrom').data.mean()
+cont2 = sp1.slice(6380, 6500, units='angstrom').data.mean()
+
+
+velo = 200
+
+spectra1 = []
+spectra2 = []
+for line in wavelengths:
+    x = sp1.xarr.as_unit('km/s', center_frequency=line, center_frequency_units='angstroms')
+    ssl085 = sp1.slice(x.x_to_pix(-velo), x.x_to_pix(velo))
+    ssl085.xarr.convert_to_unit('km/s', center_frequency=line, center_frequency_units='angstroms')
+    spectra1.append(ssl085)
+    ssl085.specfit()
+
+    x = sp2.xarr.as_unit('km/s', center_frequency=line, center_frequency_units='angstroms')
+    ssl125 = sp2.slice(x.x_to_pix(-velo), x.x_to_pix(velo))
+    ssl125.xarr.convert_to_unit('km/s', center_frequency=line, center_frequency_units='angstroms')
+    spectra2.append(ssl125)
+    ssl125.specfit()
+    print "{0}: 085 v={1}, 125 v={2}".format(line,
+                                             ssl085.specfit.parinfo.SHIFT0.value,
+                                             ssl125.specfit.parinfo.SHIFT0.value,)
+
+pl.figure(4).clf()
+pl.figure(5).clf()
+
+x1s = []
+d1s = []
+for ss in spectra1:
+    x1s += ss.xarr.tolist()
+    ss.data -= cont1
+    d1s += (ss.data/ss.data.sum()).tolist()
+    ss.data /= ss.data.sum()
+    ss.plotter(figure=pl.figure(4), clear=False)
+
+x2s = []
+d2s = []
+for ss in spectra2:
+    x2s += ss.xarr.tolist()
+    ss.data -= cont2
+    d2s += (ss.data/ss.data.sum()).tolist()
+    ss.data /= ss.data.sum()
+    ss.plotter(figure=pl.figure(5), clear=False)
+
+sp2new = pyspeckit.Spectrum(xarr=np.sort(x2s), data=np.array(d2s)[np.argsort(x2s)], xarrkwargs={'units':'km/s'})
+sp1new = pyspeckit.Spectrum(xarr=np.sort(x1s), data=np.array(d1s)[np.argsort(x1s)], xarrkwargs={'units':'km/s'})
+
+pl.figure(2)
+pl.clf()
+pl.plot(x1s, d1s, 'r.')
+pl.plot(x2s, d2s, 'b.')
+
+pl.figure(3).clf()
+sp1new.plotter(figure=pl.figure(3), marker='.', linestyle='none')
+sp2new.plotter(axis=sp1new.plotter.axis, clear=False, color='b', marker='.', linestyle='none')
+sp1new.specfit()
+sp2new.specfit()
+print sp1new.specfit.parinfo
+print sp2new.specfit.parinfo
--- muse/vmap_air.py
+++ muse/vmap_air.py
@@ -2,6 +2,7 @@ from astropy import units as u
 import numpy as np
 import spectral_cube
 import pyspeckit
+from spectral_cube.spectral_axis import vac_to_air
 
 cube = spectral_cube.SpectralCube.read('CUBEec_nall.fits', hdu=1)
 cont = cube.spectral_slab(6380*u.AA, 6500*u.AA).apply_numpy_function(np.mean, axis=0)
@@ -9,13 +10,44 @@ cube._data -= cont
 
 # Header says AWAV: air wavelengths.  It also says the coordinates are barycentric.
 # V_LSR = V_helio - 18 km/s according to Garcia-Diaz et al 2008
-wavelengths = spectral_cube.spectral_axis.vac_to_air(np.array([6302.046,
-    6313.8,  6365.536, 6549.85, 6564.61, 6585.28, 6679.99556,  6718.29,
-    6732.67])*u.AA)
+# These are the lines' vacuum wavelengths
+lines = {'OI6300': 6302.046,
+         'SIII6313': 6313.8,
+         'OI6363': 6365.536,
+         'NII6548': 6549.85,
+         'HAlpha': 6564.61,
+         'NII6584': 6585.28,
+         'HeI6678': 6679.99556,
+         'SII6716': 6718.29,
+         'SII6731': 6732.67,
+}
+airlines = {line: vac_to_air(wl*u.AA) for line,wl in lines.items()}
+#wavelengths = spectral_cube.spectral_axis.vac_to_air(np.array(lines.values())*u.AA)
 # 5877.25, 7064.21, 7137.8,  7320.94, 7331.68
 
-slabs = [cube.with_spectral_unit(u.km/u.s, 'optical', wl).spectral_slab(-200*u.km/u.s, 200*u.km/u.s)
-         for wl in wavelengths]
+wlslabs = {line:
+           cube.spectral_slab(wl-5*u.AA, wl+6*u.AA)
+           for line,wl in airlines.items()}
+
+for line,slab in wlslabs.items():
+    mom1 = slab.moment1(axis=0)
+    mom1.write('moment1_{0}_angstroms.fits'.format(line), overwrite=True)
+mean_moment = np.mean([fits.getdata('moment1_{0}_angstroms.fits'.format(line))-airlines[line].value
+                       for line in wlslabs], axis=0)
+fits.PrimaryHDU(data=mean_moment, header=fits.getheader('moment1_{0}_angstroms.fits'.format(line))).writeto('moment1_mean_deltaangstroms.fits')
+
+# Cube velocity conversion should use vacuum wavelengths
+slabs = {line:
+         cube.with_spectral_unit(u.km/u.s, 'optical', wl)
+             .spectral_slab(-200*u.km/u.s, 250*u.km/u.s)
+         for line,wl in lines.items()}
+
+for line,slab in slabs.items():
+    mom1 = slab.moment1(axis=0)
+    mom1.write('moment1_{0}_kms.fits'.format(line), overwrite=True)
+mean_moment = np.mean([fits.getdata('moment1_{0}_kms.fits'.format(line)) for
+                       line in slabs], axis=0)
+fits.PrimaryHDU(data=mean_moment, header=fits.getheader('moment1_{0}_kms.fits'.format(line))).writeto('moment1_mean_kms.fits')
 
 newcube_shape = (sum(s.shape[0] for s in slabs),) + slabs[0].shape[1:]
 newcube_spaxis = np.concatenate([s.spectral_axis for s in slabs]).value*u.km/u.s
