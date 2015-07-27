e9edbc04e36337f4b34a3c672e9af238f46e3432
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-07-27 17:45:45 +0200

    orion pretty picture making

--- GEMS/makepp_support.py
+++ GEMS/makepp_support.py
@@ -1,31 +1,31 @@
 #pylint: disable-all
-from pylab import *
+import numpy as np
 import os
 import AG_fft_tools
 import agpy
 import astropy.nddata
-import pyfits
+from astropy.io import fits as pyfits
 import time
 import aplpy
 import pyavm
 t0 = time.time()
 
-import psi.process
+#import psi.process
 
-def get_mem():
-    pid = psi.process.ProcessTable()[os.getpid()]
-    return pid.vsz
+#def get_mem():
+#    pid = psi.process.ProcessTable()[os.getpid()]
+#    return pid.vsz
 
-os.chdir('/Volumes/disk4/orion/GEMS_mosaics/')
+os.chdir('/Users/adam/work/orion/GEMS')
 
 import pyregion
 stars = pyregion.open('ks_NS_catalog.reg')
-trapstars = pyregion.open('trapstars.reg')
+#trapstars = pyregion.open('trapstars.reg')
 header = pyfits.getheader('big_mosaic_feii.fits')
 wcs=astropy.wcs.WCS(header)
 star_coords = np.array([wcs.wcs_world2pix([s.coord_list],0) for s in stars]).squeeze()
-trapstarcoords = np.array([wcs.wcs_world2pix([s.coord_list[:2]],0) for s in trapstars]).squeeze()
-trapstarradii = np.array([1.5 * s.params[2].degree / wcs.wcs.cd[1,1] for s in trapstars])
+#trapstarcoords = np.array([wcs.wcs_world2pix([s.coord_list[:2]],0) for s in trapstars]).squeeze()
+#trapstarradii = np.array([1.5 * s.params[2].degree / wcs.wcs.cd[1,1] for s in trapstars])
 
 avm = pyavm.AVM.from_header(header)
 
@@ -43,12 +43,12 @@ def mask_image(image, star_coords=star_coords, downsample=1, radius=50):
         y /= d
         mask = ((xx[y-r:y+r,x-r:x+r]-x)**2+(yy[y-r:y+r,x-r:x+r]-y)**2) < (r)**2
         image[y-r:y+r,x-r:x+r][mask] = np.nan
-    for (x,y),r in zip(trapstarcoords,trapstarradii):
-        x /= d
-        y /= d
-        r /= d
-        mask = ((xx[y-r:y+r,x-r:x+r]-x)**2+(yy[y-r:y+r,x-r:x+r]-y)**2) < (r)**2
-        image[y-r:y+r,x-r:x+r][mask] = np.nan
+    #for (x,y),r in zip(trapstarcoords,trapstarradii):
+    #    x /= d
+    #    y /= d
+    #    r /= d
+    #    mask = ((xx[y-r:y+r,x-r:x+r]-x)**2+(yy[y-r:y+r,x-r:x+r]-y)**2) < (r)**2
+    #    image[y-r:y+r,x-r:x+r][mask] = np.nan
     return image
 
 
