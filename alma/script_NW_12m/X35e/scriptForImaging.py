5c1bcd042b74c5f748b79a15c45a7c2dc6c7ec87
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-07 08:41:24 +0200

    make the big cube!

--- alma/script_NW_12m/X35e/scriptForImaging.py
+++ alma/script_NW_12m/X35e/scriptForImaging.py
@@ -963,6 +963,62 @@ clean(vis=linevis,
 
 #niter = 6000, rms 6 mJy
 
+molecules = {'CH3OCHO7(3,5)-6(1,6)E': 217.23756,
+            }
+
+spws = {0: '0,4,8,12,16,20',
+        1: '1,5,9,13,17,21',
+        2: '2,6,10,14,18,22',
+        3: '3,7,11,15,19,23',
+       }
+nchans_total = {0: 1920, 1: 3840, 2: 3840, 3: 3840}
+ncubes_per_window = 20
+finalvis='calibrated_final.ms'
+linevis = finalvis+'.contsub'
+
+for spwnum in '0123':
+    print "# running clean on all lines in spw{0}".format(spwnum)
+    spw = spws[int(spwnum)]
+    nchans_total_thiscube = nchans_total[int(spwnum)]
+    nchans_per_cube = nchans_total_thiscube/ncubes_per_window
+    inputvis = linevis
+    for ii in range(ncubes_per_window):
+        start = nchans_per_cube*ii
+        end = nchans_per_cube*(ii+1)
+        output = 'piece_of_full_orion_cube_NW.spw{0}.channels{1}to{2}'.format(spwnum, start, end)
+        #---------------------------------------------------
+        # LINE IMAGING (MOSAIC MODE)
+        if not os.path.exists(output+".image"):
+            print "Imaging {0}".format(output)
+            os.system('rm -rf ' + output + '*')
+            clean(vis = inputvis,
+                  imagename = output,
+                  field = field,
+                  spw = spw,
+                  imagermode = 'mosaic',
+                  mode = 'channel',
+                  width = 1,
+                  start = start,
+                  nchan = nchans_per_cube,
+                  chaniter = True,
+                  outframe = 'LSRK',
+                  interactive = F,
+                  niter = 2000,
+                  imsize = imsize,
+                  cell = cell,
+                  weighting = 'briggs',
+                  phasecenter = phasecenter,
+                  robust = 0.5,
+                  threshold = threshold,
+                  pbcor = F,
+                  usescratch= T)
+          
+        if not os.path.exists(output+".image.pbcor"):
+            myimagebase = output
+            impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.flux', outfile=myimagebase+'.image.pbcor', overwrite=True)
+            exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', overwrite=True)
+            exportfits(imagename=myimagebase+'.flux', fitsimage=myimagebase+'.flux.fits', overwrite=True)
+
 
 # If interactively cleaning (interactive=True), then note number of
 # iterations at which you stop for the PI. This number will help the
