44c28a1dd0bcebf9a42a114c05fbc043436d5369
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-10-11 15:33:13 +0200

    updated imaging and spectral extraction as cubes are being made

--- alma/script_NW_12m/X35e/extract_sample_spectra.py
+++ alma/script_NW_12m/X35e/extract_sample_spectra.py
@@ -3,7 +3,7 @@ from spectral_cube import SpectralCube
 
 regions = pyregion.open('box_for_diffuse_line_hunt.reg')
 
-for spwnum in (0,1):
+for spwnum in (0,1,2):
     cube1 = SpectralCube.read('Orion_NW_12m_spw{0}_lines.fits'.format(spwnum))
 
     for reg in regions:
