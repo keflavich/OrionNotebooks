232ce328a71878bbf1c1ca56502dcf20f5ac8308
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-08-02 22:54:38 +0200

    change center for pv slices

--- alma/code/pv_slices.py
+++ alma/code/pv_slices.py
@@ -1,3 +1,6 @@
+"""
+Run from parent directory, make sure pvdiagrams/ and FITS/ exist as subdirs
+"""
 import os
 import numpy as np
 from numpy import sin,cos
@@ -14,7 +17,9 @@ angles = np.linspace(0,359,nrot)
 angles_rad = angles * np.pi/180.
 
 dx = 1*u.arcmin
-center = coordinates.SkyCoord('5:35:14.5 -5:22:23', unit=(u.hour, u.deg),
+#oldcenter = coordinates.SkyCoord('5:35:14.5 -5:22:23', unit=(u.hour, u.deg),
+#                              frame='fk5')
+center = coordinates.SkyCoord('5:35:14.360 -5:22:28.70', unit=(u.hour, u.deg),
                               frame='fk5')
 sourcei = coordinates.SkyCoord('05:35:14.5121 -05 22 30.521', unit=(u.hour,
                                                                     u.deg),
@@ -28,7 +33,7 @@ paths = [ coordinates.SkyCoord([center.ra+dx*cos(theta),
          for theta in angles_rad ]
 
 
-cube = SpectralCube.read('ALMA_Outflow_b6_12M_12CO.fits')
+cube = SpectralCube.read('FITS/ALMA_Outflow_b6_12M_12CO.fits')
 mx = cube.max(axis=0)
 
 pl.close(1)
@@ -54,6 +59,7 @@ for ii,(path,theta) in enumerate(zip(paths, angles)):
 
     F.save('pvdiagrams/NE_rotation_{0:04d}.png'.format(ii), dpi=300)
 
+# command will be run from within pvdiagrams/
 cmd = "ffmpeg -y -i NE_rotation_%04d.png -c:v libx264 -pix_fmt yuv420p -vf scale=1024:768 -r 10 rotation_movie.mp4"
 import subprocess
 p = subprocess.Popen(cmd.split(), cwd='pvdiagrams/')
@@ -79,13 +85,16 @@ pl.figure(1, figsize=(12,12)).clf()
 F = aplpy.FITSFigure(hdu, figure=pl.figure(1), subplot=[0.1, 0.6, 0.8, 0.35])
 F.show_grayscale()
 F.set_title("Rotation {0:0.4g} degrees".format(theta))
+F2.save('pvdiagrams/pvslice_along_sourcei_n.png')
     
 
 F2 = aplpy.FITSFigure(mx.hdu, figure=pl.figure(1), subplot=[0.25,0.05, 0.5, 0.45])
 F2.show_grayscale()
 F2.show_lines([np.array([p.ra.deg, p.dec.deg])], color='r')
 F2.recenter(center.ra, center.dec, radius=(1.1*u.arcmin).to(u.deg).value)
-for x in patches: F2._ax1.add_patch(x)
+for x in patches:
+    F2._ax1.add_patch(x)
 F2.show_markers(sourcei.ra.deg, sourcei.dec.deg, color='g', zorder=1000,
                 edgecolor='g',
                 marker='x')
+F2.save('pvdiagrams/cubemax_with_markers.png')
